dca63eee136e5d386f732975a5783f01c4702533
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-11-17 19:05:48 +0200

    I added the BaseProfile for todo web application.

--- todo/settings.py
+++ todo/settings.py
@@ -1,6 +1,16 @@
 from fantastico.settings import BasicSettings
 
 class BaseProfile(BasicSettings):
-    '''This is your project base profile. Read documentation in order to customize it.'''
-    
-    pass
+    '''todo web application base profile.'''
+
+    @property
+    def database_config(self):
+        '''This property is automatically invoked by fantastico in order to connect to database.'''
+
+        db_config = super(BaseProfile, self).database_config
+
+        db_config["database"] = "tododb"
+        db_config["username"] = "todo_user"
+        db_config["password"] = "12345"
+
+        return db_config
