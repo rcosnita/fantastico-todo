245ec72c5f7babb0d279dbf59a966745d2334abf
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-12-07 11:12:22 +0200

    I added the correct tracking code for fantastico todo tutorial.

--- todo/sql/create_data.sql
+++ todo/sql/create_data.sql
@@ -19,12 +19,12 @@ DELETE FROM tracking_codes WHERE provider = 'Google Analytics';
 INSERT INTO tracking_codes(provider, script)
 SELECT 'Google Analytics', '
 		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
 		
-		  ga('create', 'UA-42788125-2', 'scrum-expert.ro');
-		  ga('send', 'pageview');
+		  ga("create", "UA-42788125-2", "scrum-expert.ro");
+		  ga("send", "pageview");
 		
 		</script>';
